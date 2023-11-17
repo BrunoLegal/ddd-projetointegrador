474a4d862d39c3706d1a739960f6a46e7ee5c15b
Alfredaun <alfredo.oliveira@aluno.imepac.edu.br>
2023-11-16 22:11:40 -0300

    atributos da classe Paciente

--- src/main/java/br/edu/imepac/entidades/Paciente.java
+++ src/main/java/br/edu/imepac/entidades/Paciente.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.edu.imepac.entidades;
 
+import java.util.String;
+import br.edu.imepac.entidades.Convenio
 /**
  *
  * @author Usuário
  */
 public class Paciente {
-    
+    private String nome;
+    private int RG;
+    private String orgaoEmissor;
+    private int CPF;
+    private String endereco;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private int telefone;
+    private int celular;
+    private int dataNascimento;
+    private char sexo;
+    private boolean possuiConvenio;
+    private Convenio convenio;
 }
