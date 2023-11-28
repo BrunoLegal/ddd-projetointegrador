fe04c622e51929a30488d9189dc91cbcc0d3d378
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2023-11-27 21:14:26 -0300

    Adicionado arquivo de config e uma interface

--- src/main/java/br/edu/imepac/utils/DBConfig.java
+++ src/main/java/br/edu/imepac/utils/DBConfig.java
@@ -1,2 +1,9 @@
-package br.edu.imepac.utils;public class DBConfig {
+package br.edu.imepac.utils;
+
+public class DBConfig {
+
+    public final static String url = "jdbc:mysql://localhost:3306/hospital?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
+    public final static String user = "root";
+    public final static String password = "1234";
+
 }
