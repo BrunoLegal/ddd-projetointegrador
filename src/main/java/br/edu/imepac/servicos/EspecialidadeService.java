b12f412a96217fbdc5d75edbdff41b289a7041de
Bruno Antônio Joanucci <brunonintendobr@gmail.com>
2023-11-28 16:07:10 -0300

    Começando a fazer o arquivo de serviço

--- src/main/java/br/edu/imepac/servicos/EspecialidadeService.java
+++ src/main/java/br/edu/imepac/servicos/EspecialidadeService.java
@@ -4,10 +4,26 @@
  */
 package br.edu.imepac.servicos;
 
+import br.edu.imepac.daos.EspecialidadeDao;
+import br.edu.imepac.entidades.Especialidade;
+
+import java.sql.SQLException;
+
 /**
  *
  * @author 231-001278
  */
 public class EspecialidadeService {
-    
+    private final EspecialidadeDao especialidadeDao;
+
+    public EspecialidadeService(){ this.especialidadeDao = new EspecialidadeDao(); }
+
+    public int adicionarEspecialidade(Especialidade esp) throws SQLException {
+        return this.especialidadeDao.save(esp);
+    }
+
+    public int atualizarEspecialidade(Especialidade esp) throws SQLException {
+        return this.especialidadeDao.update(esp);
+    }
+
 }
