54070f7acb951d55d0e3ffc42b451bd07b494eb4
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2023-11-27 21:23:37 -0300

    Adicionado um pacote e classe main para testar sem UI

---
+++ src/main/java/br/edu/imepac/testeMain/MainTest.java
@@ -0,0 +1,8 @@
+package br.edu.imepac.testeMain;
+
+//pacote e classe temporarios apenas para testar as classes e metodos sem utilizar o frontend
+public class MainTest {
+    public static void main(String[] args) {
+
+    }
+}
