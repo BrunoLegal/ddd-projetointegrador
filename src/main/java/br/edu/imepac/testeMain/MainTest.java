1f51e62b6c5262ceb8d1a68149b96d3f76ad0e6b
Bruno Antônio Joanucci <brunonintendobr@gmail.com>
2023-11-28 16:06:38 -0300

    Codigo modificado para acessar a database

--- src/main/java/br/edu/imepac/testeMain/MainTest.java
+++ src/main/java/br/edu/imepac/testeMain/MainTest.java
@@ -1,8 +1,24 @@
 package br.edu.imepac.testeMain;
 
+import br.edu.imepac.daos.EspecialidadeDao;
+import br.edu.imepac.entidades.Especialidade;
+
+import java.sql.SQLException;
+
 //pacote e classe temporarios apenas para testar as classes e metodos sem utilizar o frontend
 public class MainTest {
+    public final EspecialidadeDao dao = new EspecialidadeDao();
     public static void main(String[] args) {
+        Especialidade e = new Especialidade(2L,"Bombeiro");
+        EspecialidadeDao dao = new EspecialidadeDao();
+        int res;
+
+        try{
+            System.out.println(dao.update(e));
+        }catch (SQLException E){
+            E.printStackTrace();
+        }
+
 
     }
 }
