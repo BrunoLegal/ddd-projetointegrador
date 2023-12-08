d74dba5ba838b36fe335df26898fca9a0a0bde6c
Osmarindo <osmarindo.neto@aluno.imepac.edu.br>
2023-12-08 15:38:49 -0300

    convenio controller

--- src/main/java/br/edu/imepac/controladores/ConvenioController.java
+++ src/main/java/br/edu/imepac/controladores/ConvenioController.java
@@ -4,10 +4,60 @@
  */
 package br.edu.imepac.controladores;
 
-/**
- *
- * @author 231-001278
- */
+import br.edu.imepac.entidades.Convenio;
+import br.edu.imepac.servicos.ConvenioService;
+import br.edu.imepac.views.BaseForm;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ConvenioController {
-    
+    private final ConvenioService convenioService;
+    private final BaseForm baseForm;
+
+    public ConvenioController(BaseForm baseForm){
+        this.convenioService = new ConvenioService();
+        this.baseForm = baseForm;
+    }
+
+    public void addConvenio(Convenio user){
+        try{
+            this.convenioService.addConvenio(user);
+        }catch(SQLException e){
+            baseForm.showErrorInternal(e.getMessage());
+        }
+    }
+
+    public void updateConvenio(Convenio user){
+        try{
+            this.convenioService.updateConvenio(user);
+        }catch(SQLException e){
+            baseForm.showErrorInternal(e.getMessage());
+        }
+    }
+
+    public void deleteConvenio(Long id){
+        try{
+            this.convenioService.deleteConvenio(id);
+        }catch(SQLException e){
+            baseForm.showErrorInternal(e.getMessage());
+        }
+    }
+    public Convenio findConvenio(Long id){
+        try{
+            return this.convenioService.findConvenio(id);
+        }catch(SQLException e){
+            baseForm.showErrorInternal(e.getMessage());
+        }
+        return null;
+    }
+
+    public ArrayList findAllConvenio() {
+        try {
+            return this.convenioService.findAllConvenio();
+        } catch (SQLException e) {
+            baseForm.showErrorInternal(e.getMessage());
+        }
+        return null;
+    }
 }
