b7b14c62ed02fb6e9a9d3398b18051e7bf7895ae
GodOrigem <vbarcelos816@gmail.com>
2023-12-11 20:38:14 -0300

    11.12.2023 20:38

--- src/main/java/br/edu/imepac/utils/DBConfig.java
+++ src/main/java/br/edu/imepac/utils/DBConfig.java
@@ -4,6 +4,6 @@ public class DBConfig {
 
     public final static String url = "jdbc:mysql://localhost:3306/Hospital?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
     public final static String user = "root";
-    public final static String password = "1234";
+    public final static String password = "12345678";
 
 }
