7f4f47f8a52ebd9becef72c8540c1ce939e6fe8e
Alfredaun <alfredo.oliveira@aluno.imepac.edu.br>
2023-12-11 22:00:59 -0300

    PacienteServices

--- src/main/java/br/edu/imepac/servicos/PacienteService.java
+++ src/main/java/br/edu/imepac/servicos/PacienteService.java
@@ -4,10 +4,41 @@
  */
 package br.edu.imepac.servicos;
 
+import br.edu.imepac.DAOs.PacienteDao;
+import br.edu.imepac.entidades.Paciente;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
- * @author 231-001278
+ * @author Alfredo
  */
 public class PacienteService {
-    
+
+    private final PacienteDao pacienteDao;
+
+    public PacienteService() {
+        this.pacienteDao = new PacienteDao();
+    }
+
+    public int addPaciente(Paciente paciente) throws SQLException {
+        return this.pacienteDao.save(paciente);
+    }
+
+    public Paciente findPaciente(Long id) throws SQLException {
+        return this.pacienteDao.read(id);
+    }
+
+    public ArrayList<Paciente> findAllPaciente() throws SQLException {
+        return this.pacienteDao.findAll();
+    }
+
+    public int removePaciente(Long id) throws SQLException {
+        return this.pacienteDao.delete(id);
+    }
+
+    public int updatePaciente(Paciente paciente) throws SQLException {
+        return this.pacienteDao.update(paciente);
+    }
 }
